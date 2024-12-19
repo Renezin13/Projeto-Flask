993374305f5ecdc358082015c4ba99b007fd4303
João Lucas <johndescraft@gmail.com>
2024-12-19 00:11:04 -0300

    update projeto

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 # Caminho para o diretório do seu projeto
-sys.path.insert(0, '/var/www/Projeto-Flask')
+sys.path.insert(0, '/home/joaoeandre/Projeto-Flask')
 
 # Caminho para o ambiente virtual (não precisa do 'activate_this.py')
-activate_this = '/var/www/Projeto-Flask/env/bin/activate_this.py'
+activate_this = '/home/joaoeandre/Projeto-Flask/env/bin/activate_this.py'
 
 # Importa o aplicativo Flask
 from app import app as application
